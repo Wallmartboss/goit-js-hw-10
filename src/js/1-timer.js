340e2cc00950ca47d7530d5cb4da481f879cfb39
Yuri <wall.mart.boss@proton.me>
2024-04-29 23:59:15 +0300

    ...

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,8 +1,14 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+let userSelectedDate;
+
+const selectedDate = document.querySelector("#datetime-picker");
+flatpickr(selectedDate, {});
+
 const options = {
   enableTime: true,
+  dateFormat: "Y-m-d H:i",
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
