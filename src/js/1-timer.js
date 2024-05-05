0ef5a08d031bdecf4c433240dda87217b025610f
Yuri <wall.mart.boss@proton.me>
2024-05-05 03:16:48 +0300

    qq

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,21 +1,79 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
 
 let userSelectedDate;
+let inputedDate;
+let timerTime;
+let days;
+let hours;
+let minutes;
+let seconds;
 
 const selectedDate = document.querySelector("#datetime-picker");
-flatpickr(selectedDate, {});
+const button = document.querySelector("button[data-start]");
+
+const timerDays = document.querySelector(".value[data-days]");
+const timerHours = document.querySelector(".value[data-hours]");
+const timerMinutes = document.querySelector(".value[data-minutes]");
+const timerSeconds = document.querySelector(".value[data-seconds]");
+
+button.disable = true;
 
 const options = {
   enableTime: true,
-  dateFormat: "Y-m-d H:i",
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
-  },
+    inputedDate = selectedDates[0].getTime();
+    console.log(inputedDate);
+    if (inputedDate < Date.now()) {
+      iziToast.show({
+        title: 'Warning!',
+        message: 'Please choose a date in the future',
+        position: `topCenter`,
+    });
+      button.disable = true;
+      // button.style.cursor = "not-allowed"; 
+      // button.style.backgroundColor =  "#cfcfcf";
+      // button.style.color =  "#989898";
+    }  else {
+     userSelectedDate = selectedDates[0];
+     button.disable = false;
+    //  button.style.cursor = "pointer";
+    //  button.style.backgroundColor =  "#4e75ff"; 
+    //  button.style.color =  "#fff";
+    }
+    },
+  };
+
+flatpickr(selectedDate, options);
+const startTimer = button.addEventListener("click", onClick);
+
+function onClick ()  {
+  timerTime = inputedDate - Date.now();
+  const setUserTimerId = setInterval(fillingTimers(timerTime), 1000);
+  
 };
+
+function fillingTimers(value) {
+  // for (let i = value; i>0; i-=1000) { 
+  convertMs(value);
+  timerDays.textContent = `${days}`;
+  timerHours.textContent = `${hours}`;
+  timerMinutes.textContent =`${minutes}`;
+  timerSeconds.textContent = `${seconds}`;
+  // value -= 1000;
+  // };
+};
+
+function addLeadingZero(value) {
+    String(value).padStart(2,0);
+ }
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -25,13 +83,14 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+   days =  addLeadingZero(Math.floor(ms / day));
+   console.log(days);
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+   hours =  addLeadingZero(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+   minutes =  addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+   seconds =  addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
@@ -40,5 +99,3 @@ function convertMs(ms) {
 // console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 // console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
