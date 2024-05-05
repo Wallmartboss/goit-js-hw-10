83e6111b054227e4f99126dacd39a33a5b016c6e
Yuri <wall.mart.boss@proton.me>
2024-05-05 14:37:06 +0300

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,7 +3,6 @@ import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-
 let userSelectedDate;
 let inputedDate;
 let timerTime;
